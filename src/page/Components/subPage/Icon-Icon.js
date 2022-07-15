e2d840845eb5defdb47de67e83aabd7ba197ccec
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-07-15 23:23:17 +0100

    Icon component update

---
+++ src/page/Components/subPage/Icon-Icon.js
@@ -0,0 +1,244 @@
+import * as mi from "react-icons/md";
+
+export const Icons_Icons = [
+   {
+      Id: 1,
+      Icon: mi.MdAccountBox,
+      name: "MdAccountBox"
+   },
+   {
+      Id: 2,
+      Icon: mi.MdAddTask,
+      name: "MdAddTask"
+   },
+   {
+      Id: 3,
+      Icon: mi.MdAlarm,
+      name: "MdAlarm"
+   },
+   {
+      Id: 4,
+      Icon: mi.MdAssessment,
+      name: "MdAssessment"
+   },
+   {
+      Id: 5,
+      Icon: mi.MdAssignmentReturn,
+      name: "MdAssignmentReturn"
+   },
+   {
+      Id: 6,
+      Icon: mi.MdArrowCircleDown,
+      name: "MdArrowCircleDown"
+   },
+   {
+      Id: 7,
+      Icon: mi.MdArrowCircleUp,
+      name: "MdArrowCircleUp"
+   },
+   {
+      Id: 8,
+      Icon: mi.MdArrowRightAlt,
+      name: "MdArrowRightAlt"
+   },
+   {
+      Id: 9,
+      Icon: mi.MdAssignmentReturned,
+      name: "MdAssignmentReturned"
+   },
+   {
+      Id: 10,
+      Icon: mi.MdBackup,
+      name: "MdBackup"
+   },
+   {
+      Id: 11,
+      Icon: mi.MdBatchPrediction,
+      name: "MdBatchPrediction"
+   },
+   {
+      Id: 12,
+      Icon: mi.MdBookmark,
+      name: "MdBookmark"
+   },
+   {
+      Id: 13,
+      Icon: mi.MdCalendarViewDay,
+      name: "MdCalendarViewDay"
+   },
+   {
+      Id: 14,
+      Icon: mi.MdCheckCircleOutline,
+      name: "MdCheckCircleOutline"
+   },
+   {
+      Id: 15,
+      Icon: mi.MdCircleNotifications,
+      name: "MdCircleNotifications"
+   },
+   {
+      Id: 16,
+      Icon: mi.MdCode,
+      name: "MdCode"
+   },
+   {
+      Id: 17,
+      Icon: mi.MdCreditCardOff,
+      name: "MdCreditCardOff"
+   },
+   {
+      Id: 18,
+      Icon: mi.MdFavorite,
+      name: "MdFavorite"
+   },
+   {
+      Id: 19,
+      Icon: mi.MdFingerprint,
+      name: "MdFingerprint"
+   },
+   {
+      Id: 20,
+      Icon: mi.MdFitScreen,
+      name: "MdFitScreen"
+   },
+   {
+      Id: 21,
+      Icon: mi.MdGrade,
+      name: "MdGrade"
+   },
+   {
+      Id: 22,
+      Icon: mi.MdHorizontalSplit,
+      name: "MdHorizontalSplit"
+   },
+   {
+      Id: 23,
+      Icon: mi.MdHotelClass,
+      name: "MdHotelClass"
+   },
+   {
+      Id: 24,
+      Icon: mi.MdHttp,
+      name: "MdHttp"
+   },
+   {
+      Id: 25,
+      Icon: mi.MdHttps,
+      name: "MdHttps"
+   },
+   {
+      Id: 26,
+      Icon: mi.MdHourglassFull,
+      name: "MdHourglassFull"
+   },
+   {
+      Id: 27,
+      Icon: mi.MdInvertColors,
+      name: "MdInvertColors"
+   },
+   {
+      Id: 28,
+      Icon: mi.MdLabelImportantOutline,
+      name: "MdLabelImportantOutline"
+   },
+   {
+      Id: 29,
+      Icon: mi.MdLabelImportant,
+      name: "MdLabelImportant"
+   },
+   {
+      Id: 30,
+      Icon: mi.MdLabelOutline,
+      name: "MdLabelOutline"
+   },
+   {
+      Id: 31,
+      Icon: mi.MdLanguage,
+      name: "MdLanguage"
+   },
+   {
+      Id: 32,
+      Icon: mi.MdLeaderboard,
+      name: "MdLeaderboard"
+   },
+   {
+      Id: 33,
+      Icon: mi.MdLightbulb,
+      name: "MdLightbulb"
+   },
+   {
+      Id: 34,
+      Icon: mi.MdLineWeight,
+      name: "MdLineWeight"
+   },
+   {
+      Id: 35,
+      Icon: mi.MdLockOpen,
+      name: "MdLockOpen"
+   },
+   {
+      Id: 36,
+      Icon: mi.MdAssignmentReturn,
+      name: "MdAssignmentReturn"
+   },
+   {
+      Id: 37,
+      Icon: mi.MdLockOutline,
+      name: "MdLockOutline"
+   },
+   {
+      Id: 38,
+      Icon: mi.MdLock,
+      name: "MdLock"
+   },
+   {
+      Id: 39,
+      Icon: mi.MdLogin,
+      name: "MdLogin"
+   },
+   {
+      Id: 40,
+      Icon: mi.MdLoyalty,
+      name: "MdLoyalty"
+   },
+   {
+      Id: 41,
+      Icon: mi.MdNightlightRound,
+      name: "MdNightlightRound"
+   },
+   {
+      Id: 42,
+      Icon: mi.MdOfflineBolt,
+      name: "MdOfflineBolt"
+   },
+   {
+      Id: 43,
+      Icon: mi.MdOpacity,
+      name: "MdOpacity"
+   },
+   {
+      Id: 44,
+      Icon: mi.MdOutbond,
+      name: "MdOutbond"
+   },
+   {
+      Id: 45,
+      Icon: mi.MdOutbox,
+      name: "MdOutbox"
+   },
+   {
+      Id: 46,
+      Icon: mi.MdPaid,
+      name: "MdPaid"
+   },
+   {
+      Id: 47,
+      Icon: mi.MdPermPhoneMsg,
+      name: "MdPermPhoneMsg"
+   },
+   {
+      Id: 48,
+      Icon: mi.MdThumbUp,
+      name: "MdThumbUp"
+   }
+]
--- src/page/Components/subPage/Icon.js
+++ src/page/Components/subPage/Icon.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import HeaderComponent from '../HeaderComonent/HeaderComponent';
+import SideBarHeader from "../SideBar/SideBarHeader";
+import { Card, CardHeader, CardBody, Toast, ToastBody, Row, Col, CardDeck } from "reactstrap";
+import "./Icons.scss";
+import { Icons_Icons } from "./Icon-Icon";
+
 
 const Icons = () => {
    return(
-      <div>
-         Icons
-
+      <div className="Icons">
+         <SideBarHeader />
+         <div className="Icons-body">
+            <div className="head">
+               <HeaderComponent />
+            </div>
+            <Card className="card1">
+               <CardHeader className="card-header">
+                  <h2>Icons</h2>
+               </CardHeader>
+               <CardBody>
+                  <Row className="icon-toast">
+                     {Icons_Icons.map((icon, key) => {
+                        return (
+                           <Col sm='6' md='3' key={key}>
+                              <Toast>
+                                 <ToastBody className="toast_icon">
+                                    {<icon.Icon/>}
+                                    <h2>{icon.name}</h2>
+                                 </ToastBody>
+                              </Toast>
+                           </Col>
+                        )
+                     })}
+                  </Row>
+               </CardBody>
+            </Card>
+         </div>
          <Outlet/>
       </div>
    )
